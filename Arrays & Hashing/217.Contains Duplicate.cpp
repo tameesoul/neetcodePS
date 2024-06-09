164ed4324a8d5c4f1acef183cfbf93465b629008
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-06-09 03:06:30 +0300

    #include <algorithm>

--- Arrays & Hashing/217.Contains Duplicate.cpp
+++ Arrays & Hashing/217.Contains Duplicate.cpp
@@ -9,8 +9,8 @@ and return false if every element is distinct.
 class Solution {
 public:
     bool containsDuplicate(vector<int>&nums) {
-       sort(nums.begin(), nums.end());
-        for(int i = 0;i<nums.size()-1;i++)
+       sort(nums.begin(), nums.end()); /// nlog n 
+        for(int i = 0;i<nums.size()-1;i++) // n
             if(nums[i] == nums[i+1])
             return true;
         return false;
