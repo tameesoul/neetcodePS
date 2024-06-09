ea89a72d08996538a626a10cfe1d1daf85f8146e
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-06-09 06:39:28 +0300

    ProductOfArray

--- Arrays & Hashing/Product of Array Except Self.cpp
+++ Arrays & Hashing/Product of Array Except Self.cpp
@@ -8,7 +8,9 @@ using namespace std;
 Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-*/
+*/ 
+
+/// o(n)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -16,17 +18,17 @@ public:
         vector<int> pre(n);
         vector<int> pos(n);
         pre[0] = 1;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) { //n 
             pre[i] = pre[i - 1]*nums[i-1]; 
         }
         
         pos[n-1] =1;
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) { //n
             pos[i] = pos[i + 1] * nums[i+1];
         }
 
         vector<int>ans(n);
-        for(int i =0;i<n;i++)
+        for(int i =0;i<n;i++)//n
         {
             ans[i] = pre[i] * pos[i];
         }
