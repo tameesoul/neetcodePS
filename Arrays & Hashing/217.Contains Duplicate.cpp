b86442533cc2f17495a88089761faef9cb2f74b6
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-06-09 03:07:20 +0300

    RunTimeSet

--- Arrays & Hashing/217.Contains Duplicate.cpp
+++ Arrays & Hashing/217.Contains Duplicate.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>&nums) {
        sort(nums.begin(), nums.end()); /// nlog n 
-        for(int i = 0;i<nums.size()-1;i++) // n
+        for(int i = 0;i<nums.size()-1;i++) //n
             if(nums[i] == nums[i+1])
             return true;
         return false;
